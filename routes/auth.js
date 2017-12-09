135b89a38028bdc7d85cf10070c101741e526d70
techy <jrdavenport85@gmail.com>
2017-12-09 21:00:14 +0000

    add google token check and jwt sign to front end, basic charts, base modal

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const router = require('express').Router;
+
+
+app.post('/register', (request, response) => {
+
+
+    console.log('hello')
+
+});
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const app = express();
-const path = require('path');
 
-const mysql = require('mysql');
+const express = require('express');
 const bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
+const mysql = require('mysql');
+const path = require('path');
+const axios = require('axios');
+
+const app = express();
+const jsonParser = bodyParser.json();
+
+const jwt = require('jsonwebtoken');
 
 // serve front end
 app.use(express.static(path.join(__dirname, 'app/build')));
+
+
+
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -19,14 +26,48 @@ const connection = mysql.createConnection({
 connection.connect();
 
 
+const clientId = '969491308716-m9bj7gn6sjgc46o596o01ck6asedktle.apps.googleusercontent.com';
+const secret = '_wznYPqWY7D36UmLaghRWXrv';
+const googleAuthEndPoint = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=';
+const jwtSecret = 'mysecretjwttokenythingy'
+
+
+// auth
+app.post('/api/auth/register', jsonParser, (req, res) => {
+
+  const accessToken = req.body.accessToken;
+  const url = `${googleAuthEndPoint}${accessToken}`;
+  axios.get(url)
+  .then((response) => {
+    // token is valid
+    const { email, expires_in, user_id } = response.data;
+    const user = {
+      email,
+      expires: expires_in,
+      googleId: user_id
+    };
+    const token = jwt.sign(user, jwtSecret);
+    res.send(token);
+  })
+  .catch((error) => {
+    // no not a real token
+    res.send(error)
+  })
+
+});
+
+
+
+
+
+
 function queryDb(query) {
-  return new Promise(function(resolve, reject) {
-    
+  return new Promise(function (resolve, reject) {
+
     connection.query(query, function (error, results, fields) {
-      if(error) reject(error)
+      if (error) reject(error)
       resolve({ error, results, fields });
     });
-
   }
 
   );
@@ -36,10 +77,10 @@ const getAllPolls = () => queryDb('SELECT * FROM polls');
 const getPoll = (pollId) => queryDb(`SELECT * FROM polls WHERE poll_id = ${pollId}`);
 const deletePoll = (pollId) => queryDb(`DELETE polls WHERE poll_id = ${pollId}`);
 const getUserFromUserName = username => queryDb(`SELECT * FROM users WHERE username = 'username'`);
-const getUsersPolls = (username) => queryDb(`SELECT * FROM polls as p INNER JOIN users u ON u.user_id = p.user_id WHERE u.username = '${username}'`);
+const getUsersPolls = (username) => queryDb(`SELECT * FROM polls WHERE username = '${username}'`);
 
 
-const createPoll = ({ name ='', description = '', result = '[]', endDate = 'Null' }) =>
+const createPoll = ({ name = '', description = '', result = '[]', endDate = 'Null' }) =>
   queryDb(`
     INSERT INTO votingapp.polls (name, description, result, created, endDate)
     VALUES ('${name}', '${description}', '${result}', CURRENT_TIMESTAMP, ${endDate})
@@ -51,8 +92,8 @@ const createPoll = ({ name ='', description = '', result = '[]', endDate = 'Null
 
 /* get poll by id */
 app.get('/api/poll/:pollId', (req, res) => getPoll(req.params.pollId)
-.then((pollsResponse) => res.send(pollsResponse.results))
-.catch((error) => res.send(error)));
+  .then((pollsResponse) => res.send(pollsResponse.results))
+  .catch((error) => res.send(error)));
 
 /* delete poll by id */
 app.delete('/api/poll/:pollId', () => {
@@ -70,8 +111,8 @@ app.post('/api/poll', jsonParser, (req, res) => createPoll(req.body)
 
 /* get all polls */
 app.get('/api/allPolls', (req, res) => getAllPolls()
-.then((pollsResponse) => res.send(pollsResponse.results))
-.catch((error) => res.send(error)));
+  .then((pollsResponse) => res.send(pollsResponse.results))
+  .catch((error) => res.send(error)));
 
 /* get all polls for user with :username */
 app.get('/api/usersPolls/:username', (req, res) => getUsersPolls(req.params.username)
@@ -83,19 +124,25 @@ app.get('/api/usersPolls/:username', (req, res) => getUsersPolls(req.params.user
 app.post('api/poll/addOption', jsonParser, (req, res) => {
 
   getPoll(req.params.pollId)
-  .then(pollsResponse => {
+    .then(pollsResponse => {
 
-    const item = pollsResponse.results;
+      const item = pollsResponse.results;
 
-  })
-  
+    })
+
 
 });
 
 
 
 
-const server = app.listen(8080, function () {
-  console.log('Listening :)');
+
+
+const PORT = 8080;
+const server = app.listen(PORT, function () {
+  console.log(`Listening on port: ${PORT}`);
   // server.close(function () { console.log('Doh :('); });
 });
+
+
+
